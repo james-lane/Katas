3d24ddc537f6256083cac235f89e43531353532a
James Lane <4426937+james-lane@users.noreply.github.com>
2021-12-10 18:58:25 +0000

    Day1-Part2

--- src/AdventOfCode2021/day1.ts
+++ src/AdventOfCode2021/day1.ts
@@ -11,3 +11,20 @@ export const part1 = (numbers: string) => {
 
   return amountOfIncreases
 }
+
+export const part2 = (numbers: string) => {
+  const numbersArray = numbers.split(/\r?\n/).map((e) => e.trim())
+  let amountOfIncreases = 0
+  numbersArray.forEach((currentNumber, index, numbers) => {
+    const previousNumber = numbers[index - 1]
+
+    const currentArrayTotal = parseInt(currentNumber) + parseInt(numbers[index + 1]) + parseInt(numbers[index + 2])
+    const previousArrayTotal = parseInt(numbers[index - 1]) + parseInt(numbers[index]) + parseInt(numbers[index + 1])
+
+    if (currentArrayTotal > previousArrayTotal) {
+      amountOfIncreases++
+    }
+  })
+
+  return amountOfIncreases
+}
