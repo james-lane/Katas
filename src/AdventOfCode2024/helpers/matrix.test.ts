b28013a091b2310480b09822d5de876189e83485
James Lane <4426937+james-lane@users.noreply.github.com>
2024-12-06 00:29:24 +0000

    Update matrix helpers to include diagonals

--- src/AdventOfCode2024/helpers/matrix.test.ts
+++ src/AdventOfCode2024/helpers/matrix.test.ts
@@ -1,10 +1,34 @@
-import { getHorizontals, getReverseHorizontals, getVerticals, getReversedVerticals } from './matrix'
+import {
+  createMatrix,
+  getHorizontals,
+  getReversedHorizontals,
+  getVerticals,
+  getReversedVerticals,
+  getDiagonalsToBottomLeft,
+  getDiagonalsToBottomRight,
+  getDiagonalsToTopRight,
+  getDiagonalsToTopLeft,
+} from './matrix'
+
+const strMatrix = `ABC
+DEF
+GHI`
 
 const matrix = [
   ['A', 'B', 'C'],
   ['D', 'E', 'F'],
   ['G', 'H', 'I'],
 ]
+
+describe('createMatrix', () => {
+  it('should create a matrix from multiline string', () => {
+    expect(createMatrix(strMatrix)).toEqual([
+      ['A', 'B', 'C'],
+      ['D', 'E', 'F'],
+      ['G', 'H', 'I'],
+    ])
+  })
+})
 
 describe('getHorizontals', () => {
   it('should return horizontal values', () => {
@@ -18,7 +42,7 @@ describe('getHorizontals', () => {
 
 describe('getReversedHorizontals', () => {
   it('should return reversed horizontal values', () => {
-    expect(getReverseHorizontals(matrix)).toEqual([
+    expect(getReversedHorizontals(matrix)).toEqual([
       ['C', 'B', 'A'],
       ['F', 'E', 'D'],
       ['I', 'H', 'G'],
@@ -42,6 +66,39 @@ describe('getReversedVerticals', () => {
       ['G', 'D', 'A'],
       ['H', 'E', 'B'],
       ['I', 'F', 'C'],
+    ])
+  })
+})
+
+describe('getDiagonals', () => {
+  it('should return diagonal values', () => {
+    expect(getDiagonalsToTopLeft(matrix.map((arr) => arr.slice()))).toEqual([
+      ['G'],
+      ['H', 'D'],
+      ['I', 'E', 'A'],
+      ['F', 'B'],
+      ['C'],
+    ])
+    expect(getDiagonalsToBottomLeft(matrix.map((arr) => arr.slice()))).toEqual([
+      ['A'],
+      ['B', 'D'],
+      ['C', 'E', 'G'],
+      ['F', 'H'],
+      ['I'],
+    ])
+    expect(getDiagonalsToTopRight(matrix.map((arr) => arr.slice()))).toEqual([
+      ['I'],
+      ['H', 'F'],
+      ['G', 'E', 'C'],
+      ['D', 'B'],
+      ['A'],
+    ])
+    expect(getDiagonalsToBottomRight(matrix.map((arr) => arr.slice()))).toEqual([
+      ['C'],
+      ['B', 'F'],
+      ['A', 'E', 'I'],
+      ['D', 'H'],
+      ['G'],
     ])
   })
 })
--- src/AdventOfCode2024/helpers/matrix.ts
+++ src/AdventOfCode2024/helpers/matrix.ts
@@ -1,8 +1,12 @@
+export const createMatrix = (input: string) => {
+  return input.split('\n').map((row) => row.split(''))
+}
+
 export const getHorizontals = (input: any[][]) => {
   return input.map((row) => row)
 }
 
-export const getReverseHorizontals = (input: any[][]) => {
+export const getReversedHorizontals = (input: any[][]) => {
   return input.map((row) => row.toReversed())
 }
 
@@ -33,8 +37,8 @@ export const getVerticals = (input: any[][]) => {
 
 export const getReversedVerticals = (input: any[][]) => {
   const verticals: any[] = []
-
   let matrixWidth = 0
+
   input.forEach((row) => {
     if (row.length > matrixWidth) {
       matrixWidth = row.length
@@ -54,4 +58,96 @@ export const getReversedVerticals = (input: any[][]) => {
   }
 
   return verticals
+}
+
+export const getDiagonalsToBottomLeft = (input: any[][]) => {
+  const diagonals: any[] = []
+  let maxRow = 0
+
+  do {
+    const diagonal: any[] = []
+    input.forEach((row, i) => {
+      if (i <= maxRow) {
+        const elementToPush = row.shift()
+        elementToPush && diagonal.push(elementToPush)
+      }
+
+      if (i === input.length - 1) {
+        maxRow++
+      }
+    })
+
+    diagonals.push(diagonal)
+  } while (input.some((row) => row.length > 0))
+
+  return diagonals
+}
+
+export const getDiagonalsToBottomRight = (input: any[][]) => {
+  const diagonals: any[] = []
+  let maxRow = 0
+
+  do {
+    const diagonal: any[] = []
+    input.forEach((row, i) => {
+      if (i <= maxRow) {
+        const elementToPush = row.pop()
+        elementToPush && diagonal.push(elementToPush)
+      }
+
+      if (i === input.length - 1) {
+        maxRow++
+      }
+    })
+
+    diagonals.push(diagonal)
+  } while (input.some((row) => row.length > 0))
+
+  return diagonals
+}
+
+export const getDiagonalsToTopLeft = (input: any[][]) => {
+  const diagonals: any[] = []
+  let maxRow = 0
+
+  do {
+    const diagonal: any[] = []
+    input.toReversed().forEach((row, i) => {
+      if (i <= maxRow) {
+        const elementToPush = row.shift()
+        elementToPush && diagonal.push(elementToPush)
+      }
+
+      if (i === input.length - 1) {
+        maxRow++
+      }
+    })
+
+    diagonals.push(diagonal)
+  } while (input.some((row) => row.length > 0))
+
+  return diagonals
+}
+
+export const getDiagonalsToTopRight = (input: any[][]) => {
+  const diagonals: any[] = []
+  let maxRow = 0
+
+  do {
+    const diagonal: any[] = []
+    input.toReversed().forEach((row, i) => {
+      if (i <= maxRow) {
+        const elementToPush = row.pop()
+        elementToPush && diagonal.push(elementToPush)
+      }
+
+      if (i === input.length - 1) {
+        maxRow++
+      }
+    })
+
+    diagonals.push(diagonal)
+  } while (input.some((row) => row.length > 0))
+
+  return diagonals
 }
